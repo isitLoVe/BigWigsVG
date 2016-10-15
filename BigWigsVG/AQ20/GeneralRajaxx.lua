1bfea389f0f574e58389da71e4332d570b14b10a
isitLoVe <isitl0v3@gmail.com>
2016-10-15 15:37:57 +0200

    initial fix

--- BigWigsVG/AQ20/GeneralRajaxx.lua
+++ BigWigsVG/AQ20/GeneralRajaxx.lua
@@ -20,7 +20,11 @@ L:RegisterTranslations("enUS", function() return {
 	wave_cmd = "wave",
 	wave_name = "Wave Alert",
 	wave_desc = "Warn for incoming waves",
-
+	
+	thunder_cmd = "thunder",
+	thunder_name = "Thundercrash Alert",
+	thunder_desc = "Warn for incoming Thundercrash",
+	
 	trigger1 = "Kill first, ask questions later... Incoming!",
 	trigger2 = "?????",  -- There is no callout for wave 2 ><
 	trigger3 = "The time of our retribution is at hand! Let darkness reign in the hearts of our enemies!",
@@ -31,6 +35,10 @@ L:RegisterTranslations("enUS", function() return {
 	trigger8 = "Impudent fool! I will kill you myself!",
 	trigger9 = "Remember, Rajaxx, when I said I'd kill you last?",
 
+	thunder_trigger = "Thundercrash",
+	thunder_bar = "Thundercrash (50% HP)",
+	thunder_warn = "Thundercrash (50% HP) in 5sec",
+	
 	warn1 = "Wave 1/8",
 	warn2 = "Wave 2/8",
 	warn3 = "Wave 3/8",
@@ -41,6 +49,14 @@ L:RegisterTranslations("enUS", function() return {
 	warn8 = "Incoming General Rajaxx",
 	warn9 = "Wave 1/8", -- trigger for starting the event by pulling the first wave instead of talking to andorov
 
+	miniboss1 = "Captain Qeez",
+	miniboss2 = "Captain Tuubid",
+	miniboss3 = "Captain Drenn",
+	miniboss4 = "Captain Xurrem",
+	miniboss5 = "Major Yeggeth",
+	miniboss6 = "Major Pakkon",
+	miniboss7 = "Colonel Zerran",
+	
 } end )
 
 ----------------------------------
@@ -50,8 +66,8 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsGeneralRajaxx = BigWigs:NewModule(boss)
 BigWigsGeneralRajaxx.zonename = AceLibrary("Babble-Zone-2.2")["Ruins of Ahn'Qiraj"]
 BigWigsGeneralRajaxx.enabletrigger = andorov
-BigWigsGeneralRajaxx.toggleoptions = {"wave", "bosskill"}
-BigWigsGeneralRajaxx.revision = tonumber(string.sub("$Revision: 17293 $", 12, -3))
+BigWigsGeneralRajaxx.toggleoptions = {"wave", "thunder", "bosskill"}
+BigWigsGeneralRajaxx.revision = tonumber(string.sub("$Revision: 19010 $", 12, -3))
 
 ------------------------------
 --      Initialization      --
@@ -60,6 +76,10 @@ BigWigsGeneralRajaxx.revision = tonumber(string.sub("$Revision: 17293 $", 12, -3
 function BigWigsGeneralRajaxx:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH")
+	
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "DamageEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "DamageEvent")
+	
 	self.warnsets = {}
 	for i=1,9 do self.warnsets[L["trigger"..i]] = L["warn"..i] end
 end
@@ -72,12 +92,41 @@ function BigWigsGeneralRajaxx:CHAT_MSG_MONSTER_YELL( msg )
 	if self.db.profile.wave and msg and self.warnsets[msg] then
 		self:TriggerEvent("BigWigs_Message", self.warnsets[msg], "Urgent")
 	end
+	if self.db.profile.thunder and string.find(msg, L["trigger8"]) then
+		DEFUALT_CHAT_FRAME:AddMessage(msg)
+		self:ScheduleEvent("BigWigs_Message", 20, L["thunder_warn"], "Urgent")
+		self:TriggerEvent("BigWigs_StartBar", self, L["thunder_bar"], 25, "Interface\\Icons\\Spell_Nature_ThunderClap")
+	end
+end
+
+function BigWigsGeneralRajaxx:DamageEvent(msg)
+	if self.db.profile.thunder and string.find(msg, L["thunder_trigger"]) then
+		self:ScheduleEvent("BigWigs_Message", 20, L["thunder_warn"], "Urgent")
+		self:TriggerEvent("BigWigs_StartBar", self, L["thunder_bar"], 25, "Interface\\Icons\\Spell_Nature_ThunderClap")
+	end
 end
 
 function BigWigsGeneralRajaxx:CHAT_MSG_COMBAT_HOSTILE_DEATH(msg)
+	if self.db.profile.wave and string.find(msg, L["miniboss1"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn2"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss2"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn3"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss3"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn4"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss4"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn5"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss5"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn6"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss6"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn7"], "Urgent")
+	elseif self.db.profile.wave and string.find(msg, L["miniboss7"]) then
+		self:ScheduleEvent("BigWigs_Message", 3, L["warn8"], "Urgent")
+	end
 	if msg == string.format(UNITDIESOTHER, self:ToString()) then
-		if self.db.profile.bosskill then self:TriggerEvent("BigWigs_Message", string.format(L2["%s has been defeated"], self:ToString()), "Bosskill", nil, "Victory") end
-                BigWigs:Flawless()
+		if self.db.profile.bosskill then
+			self:TriggerEvent("BigWigs_Message", string.format(L2["%s has been defeated"], self:ToString()), "Bosskill", nil, "Victory")
+			BigWigs:Flawless()
+		end
 		self.core:ToggleModuleActive(self, false)
 		rajdead = true
 	end
